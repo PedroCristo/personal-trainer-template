343e77f41d78d0711494ac99aad12a7bdb8b8e89
Pedro Cristo <pcristo35@gmail.com>
2023-03-24 13:13:56 +0000

    Add navbar, goals, effects and about sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,31 @@
 
 $(document).ready(function () {
     $(window).scroll(function () {
-      if ($(document).scrollTop() > 120) {
-     $("h1").addClass("active")
-    } else {
-        
-        $("h1").removeClass("active")
+      if ($(document).scrollTop() > 400) {
+        $("nav").addClass("active");
+        $(".logo").addClass("active");
+      } else {
+        $("nav").removeClass("active");
+        $(".logo").removeClass("active");
     }
     });
   });
+
+// NAVBAR TOGGLER
+const openMenu = document.querySelector(".fa-bars");
+const closeMenu = document.querySelector(".fa-times");
+const showMenu = document.querySelector(".links");
+const sections = document.querySelector("section");
+const overlay = document.querySelector(".overley");
+
+openMenu.addEventListener("click", () => {
+    showMenu.classList.toggle("active");
+    sections.classList.toggle("active");
+    overlay.classList.toggle("active");
+  })
+  
+  closeMenu.addEventListener("click", () => {
+    showMenu.classList.toggle("active");
+    sections.classList.toggle("active");
+    overlay.classList.toggle("active");
+})
